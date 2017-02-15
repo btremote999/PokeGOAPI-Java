56a3f917deb8004118203e7708b1f3e63d005f2b
btremote <btremote@gmail.com>
2017-02-15 09:56:05 +0800

    patch pokehashProvider to support existing design for pokegohelper

--- library/src/main/java/com/pokegoapi/util/hash/pokehash/PokeHashProvider.java
+++ library/src/main/java/com/pokegoapi/util/hash/pokehash/PokeHashProvider.java
@@ -24,7 +24,6 @@ import com.pokegoapi.util.hash.crypto.PokeHashCrypto;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Moshi.Builder;
 
-import lombok.Setter;
 import net.iharder.Base64;
 
 import java.io.BufferedReader;
@@ -35,9 +34,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-import javax.net.ssl.HttpsURLConnection;
-
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Hash provider on latest version, using the PokeHash hashing service.
@@ -45,126 +43,130 @@ import lombok.Getter;
  */
 public class PokeHashProvider implements HashProvider {
     //	private static final String HASH_ENDPOINT = "https://pokehash.buddyauth.com/api/v121_2/hash";
-	private static final String DEFAULT_ENDPOINT = "https://pokehash.buddyauth.com/api/v125/hash";
-    private static final String HASH_ENDPOINT = "https://pokehash.buddyauth.com/api/v125/hash";
+    private static final String DEFAULT_ENDPOINT = "https://pokehash.buddyauth.com/api/v125/hash";
 
-	@Getter
-	@Setter
-	private String endpoint = DEFAULT_ENDPOINT;
+    @Getter
+    @Setter
+    public String endpoint = DEFAULT_ENDPOINT;
 
-	private static final int VERSION = 5500;
-	private static final long UNK25 = -9156899491064153954L;
+    private static final int VERSION = 5500;
+    private static final long UNK25 = -9156899491064153954L;
 
-	private static final Moshi MOSHI = new Builder().build();
-    private final String key;
-    private final String endPoint;
+    private static final Moshi MOSHI = new Builder().build();
+//    private final String key;
+//    private final String endPoint;
     private final HashApiCounterListener listener;
 
     /**
-	 * Hold the total amounts of requests per minute.
-	 */
-	@Getter
-	private final PokeHashKey key;
-	public static int totalRequests;
-	/**
-	 * Hold how many requests left per minute.
-	 */
-	@Getter
-	public static int requestsLeft;
-	/**
-	 * Hold the total time for the service (Always 60).
-	 */
-	@Getter
-	public static long rateLimitSeconds;
-	/**
-	 * When that api hash service key will end.
-	 * Unix Milliseconds time.
-	 */
-	@Getter
-	public static long expirationTimeStamp;
-	/**
-	 * When the current minute will end.
-	 * Unix Milliseconds time.
-	 */
-	@Getter
-	public static long endOfMinute;
-	private final boolean awaitRequests;
+     * Hold the total amounts of requests per minute.
+     */
+    @Getter
+    private final PokeHashKey key;
+    public static int totalRequests;
+    /**
+     * Hold how many requests left per minute.
+     */
+    @Getter
+    public static int requestsLeft;
+    /**
+     * Hold the total time for the service (Always 60).
+     */
+    @Getter
+    public static long rateLimitSeconds;
+    /**
+     * When that api hash service key will end.
+     * Unix Milliseconds time.
+     */
+    @Getter
+    public static long expirationTimeStamp;
+    /**
+     * When the current minute will end.
+     * Unix Milliseconds time.
+     */
+    @Getter
+    public static long endOfMinute;
+    private final boolean awaitRequests;
 
-	/**
-     	 * Creates a PokeHashProvider with the given key
-     	 *
-     	 * @param key      the key for the PokeHash API
-     * @param endPoint End Point of Poke Api Hash service
-     * @param listener Listener of Hash Api response
-	 * @param awaitRequest true if the API should, when the rate limit has been exceeded, wait until the current
-	 *     period ends, or false to throw a HashLimitExceededException
-	 */
-	public PokeHashProvider(PokeHashKey key, boolean awaitRequest, HashApiCounterListener listener) {
+    /**
+     * Creates a PokeHashProvider with the given key
+     *
+     * @param key          the key for the PokeHash API
+     * @param awaitRequest true if the API should, when the rate limit has been exceeded, wait until the current
+     *                     period ends, or false to throw a HashLimitExceededException
+     * @param listener     Listener of Hash Api response
+     */
     //public PokeHashProvider(String key, String endPoint, HashApiCounterListener listener) {
+    public PokeHashProvider(PokeHashKey key, String endPoint, boolean awaitRequest, HashApiCounterListener listener) {
         this.key = key;
-		this.awaitRequests = awaitRequest;
-		if (key == null || key.getKey() == null) {
-        this.endPoint = endPoint;
+        this.awaitRequests = awaitRequest;
+        this.endpoint = endPoint;
         this.listener = listener;
-        if (key == null) {
+        if (key == null || key.getKey() == null) {
             throw new IllegalArgumentException("Key cannot be null!");
         }
     }
 
-	/**
-	 * Provides a hash for the given arguments
-	 *
-	 * @param timestamp timestamp to hash
-	 * @param latitude latitude to hash
-	 * @param longitude longitude to hash
-	 * @param altitude altitude to hash
-	 * @param authTicket auth ticket to hash
-	 * @param sessionData session data to hash
-	 * @param requests request data to hash
-	 * @return the hash provider
-	 * @throws HashException if an exception occurs while providing this hash
-	 */
-	@Override
-	public Hash provide(long timestamp, double latitude, double longitude, double altitude, byte[] authTicket,
-			byte[] sessionData, byte[][] requests) throws HashException {
-		if (key.hasTested()) {
-			if (awaitRequests) {
-				try {
-					key.await();
-				} catch (InterruptedException e) {
-					throw new HashException(e);
-				}
-			} else {
-				long time = System.currentTimeMillis();
-				long timeLeft = time - key.getRatePeriodEnd();
-				if (key.getRequestsRemaining() <= 0 && timeLeft > 0) {
-					throw new HashLimitExceededException(
-							"Exceeded hash request limit! Period ends in " + timeLeft + "ms");
-				}
-			}
-		}
+    /**
+     * Provides a hash for the given arguments
+     *
+     * @param timestamp   timestamp to hash
+     * @param latitude    latitude to hash
+     * @param longitude   longitude to hash
+     * @param altitude    altitude to hash
+     * @param authTicket  auth ticket to hash
+     * @param sessionData session data to hash
+     * @param requests    request data to hash
+     * @return the hash provider
+     * @throws HashException if an exception occurs while providing this hash
+     */
+    @Override
+    public Hash provide(long timestamp, double latitude, double longitude, double altitude,
+                        byte[] authTicket,
+                        byte[] sessionData, byte[][] requests) throws HashException {
+        if (key.hasTested()) {
+            if (awaitRequests) {
+                try {
+                    key.await();
+                } catch (InterruptedException e) {
+                    throw new HashException(e);
+                }
+            } else {
+                long time = System.currentTimeMillis();
+                long timeLeft = time - key.getRatePeriodEnd();
+                if (key.getRequestsRemaining() <= 0 && timeLeft > 0) {
+                    throw new HashLimitExceededException(
+                            "Exceeded hash request limit! Period ends in " + timeLeft + "ms");
+                }
+            }
+        }
 
+
+        Request request = new Request(latitude,
+                                      longitude,
+                                      altitude,
+                                      timestamp,
+                                      authTicket,
+                                      sessionData,
+                                      requests);
         try {
-		Request request = new Request(latitude, longitude, altitude, timestamp, authTicket, sessionData, requests);
-		try {
-			HttpURLConnection connection = (HttpURLConnection) new URL(endpoint).openConnection();
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("X-AuthToken", key.getKey());
-			connection.setRequestProperty("content-type", "application/json");
-			connection.setRequestProperty("User-Agent", "PokeGOAPI-Java");
-			connection.setDoOutput(true);
+            HttpURLConnection connection = (HttpURLConnection) new URL(endpoint).openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("X-AuthToken", key.getKey());
+            connection.setRequestProperty("content-type", "application/json");
+            connection.setRequestProperty("User-Agent", "PokeGOAPI-Java");
+            connection.setDoOutput(true);
 
-			String requestJSON = MOSHI.adapter(Request.class).toJson(request);
-			DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-			out.writeBytes(requestJSON);
-			out.flush();
-			out.close();
+            String requestJSON = MOSHI.adapter(Request.class).toJson(request);
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            out.writeBytes(requestJSON);
+            out.flush();
+            out.close();
 
-			int responseCode = connection.getResponseCode();
+            int responseCode = connection.getResponseCode();
 
-			this.key.setProperties(connection);
+            this.key.setProperties(connection);
 
-			String error = getError(connection);
+            String error = getError(connection);
 
             switch (responseCode) {
                 case HttpURLConnection.HTTP_OK:
@@ -175,7 +177,8 @@ public class PokeHashProvider implements HashProvider {
                         builder.append(line);
                     }
                     in.close();
-                    Response response = MOSHI.adapter(Response.class).fromJson(builder.toString());
+                    Response response = MOSHI.adapter(Response.class)
+                            .fromJson(builder.toString());
                     long locationAuth = response.getLocationAuthHash();
                     long location = response.getLocationHash();
                     int locationAuthHash = (int) ((locationAuth & 0xFFFFFFFFL) ^ (locationAuth >>> 32));
@@ -183,7 +186,9 @@ public class PokeHashProvider implements HashProvider {
                     if (this.listener != null) {
                         this.listener.hashSuccess(System.currentTimeMillis() - timestamp);
                     }
-                    return new Hash(locationAuthHash, locationHash, response.getRequestHashes());
+                    return new Hash(locationAuthHash,
+                                    locationHash,
+                                    response.getRequestHashes());
                 case HttpURLConnection.HTTP_BAD_REQUEST:
                     if (error.length() > 0) {
                         if (this.listener != null) {
@@ -232,8 +237,8 @@ public class PokeHashProvider implements HashProvider {
                     }
 
                     throw new HashLimitExceededException("Exceeded hash limit!");
-				case 404:
-					throw new HashException("Unknown hashing endpoint! \"" + this.endpoint + "\"");
+                case 404:
+                    throw new HashException("Unknown hashing endpoint! \"" + this.endpoint + "\"");
                 default:
                     if (error.length() > 0) {
                         if (this.listener != null) {
@@ -280,25 +285,24 @@ public class PokeHashProvider implements HashProvider {
 
     public interface HashApiCounterListener {
         void hashFailed(long time_spend_ms, int err_no, String err_msg);
-	
+
         void hashSuccess(long time_spend_ms);
     }
-    
+
     @Override
-	public int getHashVersion() {
-		return VERSION;
-	}
+    public int getHashVersion() {
+        return VERSION;
+    }
 
-	@Override
-	public Crypto getCrypto() {
-		return PokeHashCrypto.POKE_HASH;
-	}
-	
-	@Override
-	public long getUNK25() {
-		return UNK25;
-	}
+    @Override
+    public Crypto getCrypto() {
+        return PokeHashCrypto.POKE_HASH;
+    }
 
+    @Override
+    public long getUNK25() {
+        return UNK25;
+    }
 
 
     private static class Response {
